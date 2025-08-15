bacb8be04e1eb8453d4f43e9dbaf636499f816b8
jebbane mohammed <jebbanelab@gmail.com>
2025-08-15 21:33:49 +0200

    Mise à jour main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_route_table" "RT" {
   vpc_id = aws_vpc.myvpc.id 
 
   route {
-    cidr_block = "0.0.0.0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 }
